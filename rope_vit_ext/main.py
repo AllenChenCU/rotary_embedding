1f2815cf79ca47c2592b81383efc6c971902f1f2
Allen Chen <atc2160@columbia.edu>
2024-11-14 20:49:40 -0800

    minor fix

--- rope_vit_ext/main.py
+++ rope_vit_ext/main.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     parser.add_argument('--dist_url', default='tcp://127.0.0.1:23456', help='url used to set up distributed training') #'env://'
     parser.add_argument('--repeated-aug', action='store_true', default=False)
     parser.add_argument('--no-repeated-aug', action='store_false', dest='repeated_aug')
-    parser.set_defaults(repeated_aug=True)
+    #parser.set_defaults(repeated_aug=True)
     parser.add_argument('--lr', type=float, default=5e-4, metavar='LR', help='learning rate (default: 5e-4)')
     parser.add_argument('--unscale-lr', action='store_true')
     args = parser.parse_args()
