63e11f18c13099d9d82bfbd7547e95daf9dbb72a
Allen Chen <atc2160@columbia.edu>
2024-11-18 12:34:06 -0800

    minor fix

--- rope_vit_ext/data.py
+++ rope_vit_ext/data.py
@@ -44,7 +44,13 @@ def build_transform(is_train, input_size=32):
 def build_dataset(is_train, args):
 
     if args.dataset == 'cifar10':
-        transform = build_transform(is_train, input_size=args.input_size)
+        transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        #transform = build_transform(is_train, input_size=args.input_size)
         dataset = datasets.CIFAR10(args.data_path, download=True, train=is_train, transform=transform)
         nb_classes = 10
     elif args.dataset == 'imagenet':
